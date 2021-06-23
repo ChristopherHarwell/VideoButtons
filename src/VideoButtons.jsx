6050c5dd81f4cf6cf8f011dbc56947dd5d0c80b8
Christopher Harwell <45679995+ChristopherHarwell@users.noreply.github.com>
2021-06-23 10:30:47 -0400

    first commit

---
+++ src/VideoButtons.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function VideoButtons() {
+    return (
+        <>
+            <div  className='test1'>
+                <a href='https://google.com' className='componentOne'>
+                    English
+                </a>
+                <a href='https://google.com' className='componentTwo'>
+                    English CC
+                </a>
+            </div>
+
+            <div className='test2'>
+                <a href='https://google.com' className='componentThree'>
+                    Spanish CC
+                </a>
+                <a href='https://google.com' className='componentFour'>
+                    French CC
+                </a>
+            </div>
+        </>
+    )
+}
