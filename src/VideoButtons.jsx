19dd4c306f2de295d15f280883b86253a4dbab27
Christopher Harwell <45679995+ChristopherHarwell@users.noreply.github.com>
2021-06-23 14:53:17 -0400

    Adds Test iframe for video

--- src/VideoButtons.jsx
+++ src/VideoButtons.jsx
@@ -4,20 +4,20 @@ export default function VideoButtons() {
     return (
         <>
             <div  className='test1'>
-                <a href='https://google.com' className='componentOne'>
-                    English
+                <a href='https://boxcast.tv/view-embed/cc-simlive-test-867530?showTitle=0&showDescription=0&showHighlights=0&showRelated=0&defaultVideo=next&market=smb&showDocuments=0&showIndex=0&showDonations=0' className='componentOne'>
+                    <p className='text'>English</p>
                 </a>
-                <a href='https://google.com' className='componentTwo'>
-                    English CC
+                <a href='https://boxcast.tv/view-embed/cc-simlive-test-867530?showTitle=0&showDescription=0&showHighlights=0&showRelated=0&defaultVideo=next&market=smb&showDocuments=0&showIndex=0&showDonations=0' className='componentTwo'>
+                    <p className='text'>English CC</p>
                 </a>
             </div>
 
             <div className='test2'>
-                <a href='https://google.com' className='componentThree'>
-                    Spanish CC
+                <a href='https://boxcast.tv/view-embed/cc-simlive-test-867530?showTitle=0&showDescription=0&showHighlights=0&showRelated=0&defaultVideo=next&market=smb&showDocuments=0&showIndex=0&showDonations=0' className='componentThree'>
+                    <p className='text'>Spanish CC</p>
                 </a>
-                <a href='https://google.com' className='componentFour'>
-                    French CC
+                <a href='https://boxcast.tv/view-embed/cc-simlive-test-867530?showTitle=0&showDescription=0&showHighlights=0&showRelated=0&defaultVideo=next&market=smb&showDocuments=0&showIndex=0&showDonations=0' className='componentFour'>
+                    <p className='text'>French CC</p>
                 </a>
             </div>
         </>
