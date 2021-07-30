65bd58f09b65f3f7b1c1ff4d9b6f6afefe4e0859
Christopher Harwell <45679995+ChristopherHarwell@users.noreply.github.com>
2021-07-30 09:13:06 -0400

    Adding BoxCast branding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
 import VideoButtons from "./VideoButtons";
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <img className="logo" src="https://www.boxcast.com/hs-fs/hub/484866/file-2483746126-png/Logos/NewBoxCastLogo.png" alt="boxcast logo"/>
+      </div>
       <VideoButtons />
     </div>
   );
